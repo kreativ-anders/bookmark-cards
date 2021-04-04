5446233e32fd6da532dac1e3a050c43f2a5ae3fd
Manuel Steinberg <github@manuel-steinberg.de>
2021-04-04 11:49:50 +0200

    fix wordning

--- site/snippets/modals/user.php
+++ site/snippets/modals/user.php
@@ -2,7 +2,7 @@
   <div class="modal-background" onclick="$('#userModal').toggleClass('is-active');"></div>
   <div class="modal-card">
     <header class="modal-card-head">
-      <p class="modal-card-title"><i><?= $kirby->user()->email(); ?></i> | Einstellungen</p>
+      <p class="modal-card-title"><i><?= $kirby->user()->email(); ?></i> | Settings</p>
       <button onclick="$('#userModal').toggleClass('is-active');" class="delete" aria-label="close"></button>
     </header>
     <section class="modal-card-body">
@@ -13,10 +13,10 @@
           </p>
           <div class="control is-expanded">
             <input class="input" type="email" id="email" name="email" value="<?= esc(get('email')) ?>"
-            placeholder="<?= (get('email'))? get('email') : $kirby->user()->email() ?>" autocomplete="email">
+              placeholder="<?= (get('email'))? get('email') : $kirby->user()->email() ?>" autocomplete="email">
           </div>
           <div class="control">
-            <input type="submit" class="button is-primary" name="update" value="Updaten">
+            <input type="submit" class="button is-primary" name="update" value="Update">
           </div>
         </div>
       </form>
@@ -28,31 +28,34 @@
           </p>
           <div class="control is-expanded">
             <input class="input" type="password" id="password" name="password" value="<?= esc(get('password')) ?>"
-            placeholder="Password" autocomplete="new-password">
+              placeholder="Password" autocomplete="new-password">
           </div>
           <div class="control">
-            <input type="submit" class="button is-primary" name="update" value="Updaten"> 
+            <input type="submit" class="button is-primary" name="update" value="Update">
           </div>
         </div>
       </form>
     </section>
-    <footer class="modal-card-foot">  
-
+    <footer class="modal-card-foot">
       <a class="button is-info is-light" href="<?= $kirby->user()->getStripePortalURL() ?>">
         <span class="icon">
           <i class="fas fa-money-check"></i>
         </span>
-        <p>Manage Subscriptions</p>
+        <span>Manage Subscriptions</span>
       </a>
-    
-      <a href="user.json" target="_blank" class="button is-info is-light">meine Daten anfordern</a>
-
-      <form action="user" method="POST" onsubmit="return confirm('Cannot be revert!');">
-        <div class="buttons">
+      <a href="user.json" target="_blank" class="button is-info is-light">
+        <span class="icon">
+          <i class="fas fa-file-export"></i>
+        </span>
+        <span>Export Data</span>
+      </a>
+    </footer>
+    <div class="card-content">
+      <form action="user" method="POST" onsubmit="return confirm('This action cannot be revert! Are you sure?');">
+        <div class="buttons is-pulled-right">
           <input class="button is-danger" type="submit" name="delete" value="Delete Account">
         </div>
-        
       </form>
-    </footer>
+    </div>
   </div>
 </div>
