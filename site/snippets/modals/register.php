33b2727cc9cfd4f14aadb58c9b4bcc807b7b7bd2
Manuel Steinberg <github@manuel-steinberg.de>
2021-04-04 19:09:04 +0200

    add TOS checkbox

--- site/snippets/modals/register.php
+++ site/snippets/modals/register.php
@@ -7,25 +7,30 @@
     </header>
     <form action="register" method="POST">
       <section class="modal-card-body">
-          <div class="field">
-            <p class="control has-icons-left has-icons-right">
-              <input class="input" type="email" name="email" value="" placeholder="Email" required>
-              <span class="icon is-small is-left">
-                <i class="fas fa-envelope"></i>
-              </span>
-              <p class="help"><i style="color: rgba(80,80,80,.5)">Die Email Adresse wird lediglich für die Authentifizierung gespeichert und verwendet.</i> 😬</p>
-            </p>
-          </div>
-          <div class="field">
-            <p class="control has-icons-left">
-              <input class="input" type="password" name="password" value="" placeholder="Passwort" required>
-              <span class="icon is-small is-left">
-                <i class="fas fa-lock"></i>
-              </span>
-              <p class="help"><i style="color: rgba(80,80,80,.5)">Einmal das Passwort eingeben sollte ausreichen!?</i></p>
-            </p>
-          </div>
-        
+        <div class="field">
+          <p class="control has-icons-left has-icons-right">
+            <input class="input" type="email" name="email" value="" placeholder="Email" required>
+            <span class="icon is-small is-left">
+              <i class="fas fa-envelope"></i>
+            </span>
+            <p class="help"><i style="color: rgba(80,80,80,.5)">Die Email Adresse wird lediglich für die Authentifizierung gespeichert und verwendet.</i> 😬</p>
+          </p>
+        </div>
+        <div class="field">
+          <p class="control has-icons-left">
+            <input class="input" type="password" name="password" value="" placeholder="Passwort" required>
+            <span class="icon is-small is-left">
+              <i class="fas fa-lock"></i>
+            </span>
+            <p class="help"><i style="color: rgba(80,80,80,.5)">Einmal das Passwort eingeben sollte ausreichen!?</i></p>
+          </p>
+        </div>
+        <div class="field">
+          <label class="checkbox has-text-primary">
+            <input type="checkbox" name="tos" required>
+            <?= $pages->findById('register')->tos()->kirbytextinline(); ?>
+          </label>
+        </div>
       </section>
       <footer class="modal-card-foot">
         <input class="button is-success" name="register" type="submit" value="Register">     
