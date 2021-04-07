c8f1959b408dfcd14914b14dfe6e9c6c33c6c835
Manuel Steinberg <github@manuel-steinberg.de>
2021-04-07 12:58:33 +0200

    Create features.php

---
+++ site/snippets/features.php
@@ -0,0 +1,23 @@
+<section class="section">
+  <div class="container has-text-centered">
+    <h2 class="title mb-6">Some of our core values</h2>
+    <div class="columns is-multiline">
+      <div class="column is-6 is-3-desktop">
+        <h4 class="title is-4">Security</h4>
+        <p>Your data is virtually unhackable <br />since no database is used.</p>
+      </div>
+      <div class="column is-6 is-3-desktop">
+        <h4 class="title is-4">Privacy</h4>
+        <p>No activity tracking or ads! <br />You own the data.</p>
+      </div>
+      <div class="column is-6 is-3-desktop">
+        <h4 class="title is-4">Open Source</h4>
+        <p>Transparency creates trust. <br />Check out GitHub.</p>
+      </div>
+      <div class="column is-6 is-3-desktop">
+        <h4 class="title is-4">Lean</h4>
+        <p>No bloat. <br />Just bookmarks.</p>
+      </div>
+    </div>
+  </div>
+</section>
