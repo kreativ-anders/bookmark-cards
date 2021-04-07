ebb984de4e27eaa77a80bbf721595b86bc328d29
Manuel Steinberg <github@manuel-steinberg.de>
2021-04-07 12:58:37 +0200

    Create pricing.php

---
+++ site/snippets/pricing.php
@@ -0,0 +1,25 @@
+<section class="section">
+  <div class="container has-text-centered py-4">
+    <h2 class="title">Easy Pricing</h2>
+    <p class="subtitle">... hopefully</p>
+    <hr class="my-6">
+    <div class="columns is-centered">
+      <div class="column is-8">
+        <div class="columns">
+          <div class="column">
+            <h4 class="title is-5">Basic</h4>
+            <h3 class="title is-2 is-spaced">0€ <small class="has-text-grey">/ month</small></h3>
+            <p class="subtitle">Check if Bookmark.cards <br />is the right tool for you <br/>with <?= option('noPremiumLimit'); ?> bookmarks.</p>
+            <button id="register" class="button is-primary" onclick="$('#registerModal').toggleClass('is-active');"><strong>Register</strong></button>
+          </div>
+          <div class="column">
+            <h4 class="title is-5">Premium</h4>
+            <h3 class="title is-2 is-spaced">10€ <small class="has-text-grey">/ year</small></h3>
+            <p class="subtitle">You like Bookmark.cards? Awesome!</p>
+            <p>(You can switch your plan interval <br />after monthly initialisation.)</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
