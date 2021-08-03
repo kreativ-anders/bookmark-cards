b4f2048c9deda0a0e6fa68702365e45a98d3389f
Manuel Steinberg <github@manuel-steinberg.de>
2021-08-03 11:33:17 +0200

    Create hero.php

---
+++ site/snippets/hero.php
@@ -0,0 +1,11 @@
+<section class="section">
+  <div class="container has-text-centered">
+    <div class="mb-5 columns is-multiline is-centered">
+      <div class="column is-10-tablet is-6-desktop">
+        <h2 class="mt-2 mb-4 is-size-1 is-size-3-mobile has-text-weight-bold">Another Bookmark App!?</h2>
+        <p class="subtitle has-text-grey mb-5">It´s a super lean bookmarking app, which still looks pretty good.</p>
+        <div class="buttons is-centered mb-5"><a class="button is-dark is-outlined is-primary" href="#features">What makes it special?</a><a class="button is-dark is-primary" href="#pricing">Register</a></div>
+      </div>
+    </div>
+  </div>
+</section>
