d8cc362df5b4be4435fe100660d842c95dbd5aee
Manuel Steinberg <github@manuel-steinberg.de>
2021-08-03 08:56:00 +0200

    fix csv user export

--- site/templates/user.csv.php
+++ site/templates/user.csv.php
@@ -5,10 +5,19 @@ header('Content-type: text/plain; charset=UTF-8');
 $csv = "";
 
 if($kirby->user()) {
-  
-  $csv .= "Email;Langauge\n";
-  $csv .= $kirby->user()->email().";".$kirby->user()->language()."\n";
 
+  $bookmarks = $kirby->user()->bookmarks()->yaml();
+
+  $csv .= "Email;" . $kirby->user()->email() . ";\n";
+  $csv .= "password;'*********************'" . ";\n";
+  $csv .= "Subscription;" . $kirby->user()->tier()->toString() . ";\n";
+  $csv .= "\n";
+
+  $csv .= "Title;Link;Tags;\n";
+
+  foreach ($bookmarks as $bookmark) {
+    $csv .= $bookmark['title'] . ";" . $bookmark['link'] . ";" . $bookmark['tags'] . ";\n";
+  }
 
 }
 else {
