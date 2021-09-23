07993da24575a3942c8511ec3a75ac1d21f426f5
Manuel Steinberg <github@manuel-steinberg.de>
2021-09-23 11:21:11 +0200

    hero: update link to core values

--- site/snippets/hero.php
+++ site/snippets/hero.php
@@ -4,7 +4,7 @@
       <div class="column is-10-tablet is-6-desktop">
         <h2 class="mt-2 mb-4 is-size-1 is-size-3-mobile has-text-weight-bold">Another Bookmark App!?</h2>
         <p class="subtitle has-text-grey mb-5">It´s a super lean bookmarking app, which still looks pretty good.</p>
-        <div class="buttons is-centered mb-5"><a class="button is-dark is-outlined is-primary" href="#features">What makes it special?</a><a class="button is-dark is-primary" href="#pricing">Register</a></div>
+        <div class="buttons is-centered mb-5"><a class="button is-dark is-outlined is-primary" href="#values">What are the core values?</a><a class="button is-dark is-primary" href="#pricing">Register</a></div>
       </div>
     </div>
   </div>
