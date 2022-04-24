a3ee0db208ead7295dab272f49971a5abb95025d
Manuel Steinberg <github@manuel-steinberg.de>
2022-04-24 18:02:35 +0200

    Squashed commit of the following:
    commit 3de7901d8ae13569c8582a80b71ce4696c57f2ce
    Author: Manuel Steinberg <github@manuel-steinberg.de>
    Date:   Sun Apr 24 18:00:43 2022 +0200
    
        Update kirby-sitemap
    
    commit ac36c0ec9cda3597a6698e22561fb63c9e1ac414
    Author: Manuel Steinberg <github@manuel-steinberg.de>
    Date:   Sun Apr 24 17:37:18 2022 +0200
    
        📦️ add kirby-sitemap plugin
    
    commit b4b30eb55b1f104c726812a78d6fc7a79d0c7a2b
    Author: Manuel Steinberg <github@manuel-steinberg.de>
    Date:   Sun Apr 3 11:27:35 2022 +0200
    
        Update brand-names
    
    commit 1964939ee8647f914661914d992e75d7c5a15b00
    Author: Manuel Steinberg <github@manuel-steinberg.de>
    Date:   Sat Apr 2 14:20:33 2022 +0200
    
        ⚗️ hide manifest until user is logged-in
    
    commit f4b565b29fd85df51dfb5704950d02ab05e773d9
    Author: Manuel Steinberg <github@manuel-steinberg.de>
    Date:   Sat Apr 2 13:58:13 2022 +0200
    
        ➕➖ add pico and remove bulma

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -23,9 +23,11 @@
   <link rel="dns-prefetch" href="//bookmark.cards">
   <link rel="preconnect" href="//bookmark.cards">
 
-  <link rel="manifest" href="manifest.json">
-  <meta name="theme-color" content="#ffffff"/>
-  
+  <?php if ($kirby->user()): ?>
+    <link rel="manifest" href="manifest.json">
+    <meta name="theme-color" content="#ffffff"/>
+  <?php endif; ?>
+    
   <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
