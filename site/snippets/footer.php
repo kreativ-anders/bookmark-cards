ca4043ca47cd3e115597db09f44c219cf78de148
Manuel Steinberg <github@manuel-steinberg.de>
2022-08-24 19:39:24 +0200

    🔥 remove kreativ-anders logo

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -10,19 +10,6 @@
       </figure>
 
       </a>
-      Enhanced by
-      <br>
-      <a href="https://kreativ-anders.de/" target="_blank">
-        <svg xmlns="http://www.w3.org/2000/svg" style="width: 5rem" version="1.2" viewBox="0 0 225 177.873">
-          <g fill="none" fill-rule="evenodd" stroke-linecap="square" stroke-linejoin="bevel">
-            <path fill="#f5a000" d="M58.38 30.355h108.485v4.339H58.38v-4.34"/>
-            <path fill="#f5a000" d="M58.38 30.355h4.34V89.66h-4.34V30.355m0 112.824h108.485v4.34H58.38v-4.34"/>
-            <path fill="#f5a000" d="M162.526 89.66h4.34v57.858h-4.34V89.66"/>
-            <text xml:space="preserve" x="603.5" y="37" font-family="Oswald" fill="#262626" font-size="24" font-weight="400" transform="translate(-744.409 18.783) scale(1.44647)"><tspan font-family="Oswald" font-size="21.333" >anders</tspan></text>
-            <text xml:space="preserve" x="547" y="75" font-family="Oswald" fill="#262626" font-size="24" font-weight="400" transform="translate(-770.445 18.783) scale(1.44647)"><tspan font-family="Oswald" font-size="21.333">kreativ</tspan></text>
-          </g>
-        </svg>
-      </a>
     </p> 
     
   <link rel="stylesheet" href="assets/css/brands.min.css">  
