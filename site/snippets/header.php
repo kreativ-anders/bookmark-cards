38a779dcaf2b1e6d3a5a12604fc7246389ffd3e0
Manuel Steinberg <github@manuel-steinberg.de>
2022-11-18 15:50:44 +0100

    🧑‍💻 add env info in header

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -64,7 +64,7 @@
             </h1>
           </a>
           <p style="margin: 0">
-            .cards <?= e(option('debug'), " ❤ Kirby v" . Kirby::version())?>
+            .cards <?= e(option('debug'), " Kirby v" . Kirby::version() . " PHP " . phpversion())?>
             <?php if ($kirby->user() && $kirby->user()->isAllowed(option('kreativ-anders.memberkit.tiers')[1]['name'])): ?>
             <span><strong style="color: darkgoldenrod">Premium</strong></span>
             <?php endif; ?>
